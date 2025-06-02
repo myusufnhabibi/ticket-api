f3fc72b93b52697500375bcda1c3c7246ac1df15
habib <myusufnhabibi@gmail.com>
2025-06-02 21:41:52 +0700

    fixing resource user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,8 +17,8 @@ class UserResource extends JsonResource
          return [
             'id' => $this->id,
             'name' => $this->name,
-            'email' => $this->email
-            // 'role' => $this->role
+            'email' => $this->email,
+            'role' => $this->role
         ];
     }
 }
